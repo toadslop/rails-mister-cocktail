586d0790b9842190104d4e9d80f3807a4fd5e19c
Brian Heise <bnheise@gmail.com>
2020-02-21 15:29:31 +0900

    commented out some lines from application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,8 +3,8 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
+// require("@rails/ujs").start()
+// require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 import 'bootstrap';
@@ -17,6 +17,6 @@ import 'bootstrap';
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 // Support component names relative to this directory:
-var componentRequireContext = require.context("components", true);
-var ReactRailsUJS = require("react_ujs");
-ReactRailsUJS.useContext(componentRequireContext);
+// var componentRequireContext = require.context("components", true);
+// var ReactRailsUJS = require("react_ujs");
+// ReactRailsUJS.useContext(componentRequireContext);
