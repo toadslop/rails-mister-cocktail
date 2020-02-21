d3bcdddbf9a4c7cc77d25198ff3842606de974d7
Brian Heise <bnheise@gmail.com>
2020-02-21 17:26:55 +0900

    got image upload working; appropriately styled images

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -31,7 +31,7 @@ class CocktailsController < ApplicationController
   private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name, :search)
+    params.require(:cocktail).permit(:name, :search, :photo)
   end
 
   def set_cocktail
